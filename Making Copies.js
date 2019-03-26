2596ce9bf0ef79b9f8bac6cda408e177dbbe50e3
vianat <vianatdev@gmail.com>
2019-03-26 16:31:43 -0500

    Making Copies

---
+++ Making Copies.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/53d2697b7152a5e13d000b82/train/javascript
+function copyList(l){
+    let a  = [];
+    for(let i = 0;  i< l.length; i++){
+        a.push(l[i]);
+    }
+    return a;
+}
