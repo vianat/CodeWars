aaa2b31a09acb5ec953e83cf4491b74fe5fe3d23
vianat <vianatdev@gmail.com>
2019-03-27 13:31:06 -0500

    Make a function that does arithmetic!

---
+++ Make a function that does arithmetic!.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/javascript
+function arithmetic(a, b, operator){
+    if(operator === "add") return a+b;
+    if(operator === "subtract") return a-b;
+    if(operator === "multiply") return a*b;
+    if(operator === "divide") return a/b;
+}
